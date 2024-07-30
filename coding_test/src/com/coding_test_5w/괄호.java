8c89472b92be1c9f3ee7e2763fd27eb94450084b
유현준 <140572350+YOOHYUNJUNE@users.noreply.github.com>
2024-07-30 17:52:43 +0900

    0730

--- coding_test/src/com/coding_test_5w/괄호.java
+++ coding_test/src/com/coding_test_5w/괄호.java
@@ -17,8 +17,7 @@ public class 괄호 {
 		Scanner sc = new Scanner(System.in);
 		
 		// 괄호를 넣을 배열
-//		List<String> arr = new ArrayList<>();
-		List<String> arr = new LinkedList<>();
+		List<String> arr = new ArrayList<>();
 
 
 		// 괄호 입력 횟수
@@ -32,6 +31,9 @@ public class 괄호 {
 		int left = 1;
 		int right = -1;
 		
+		// NO일 경우 지나치게 하기
+		boolean yes = true;
+		
 		sc.nextLine();
 		for (int i=0; i<test; i++) {
 			System.out.println("괄호를 입력하세요.");
@@ -40,30 +42,37 @@ public class 괄호 {
 			// 시작 ) 인 경우 NO
 			if (arr.get(i).charAt(0) == ')') {
 				System.out.println("NO");
+				yes = false;
+
 				// 시작 ( 인 경우
 			} else if (arr.get(i).charAt(0) == '(') {
 				for (int j=0; j<arr.get(i).length(); j++) {
-					
-					
-					// arr.get(1)부터 )가 2번 이상 나오면 무조건 NO
-					if (arr.get(i).length() > j+2) {
-						if(arr.get(i).charAt(j+1) == ')' && arr.get(i).charAt(j+2) == ')') {
-							System.out.println("NO");
-						}
-					} 
-				
+					// 괄호 입력에 따라 + 또는 -
 					if (arr.get(i).charAt(j) =='(') {
 						left++;
 					} else if (arr.get(i).charAt(j) ==')') {
 						right--;
 					}
 					
-				}
+					
+					// arr.get(1)부터 )가 2번 이상 나오면 무조건 NO
+					if (arr.get(i).length() > j+2) {
+						if(arr.get(i).charAt(j+1) == ')' && arr.get(i).charAt(j+2) == ')') {
+							System.out.println("NO");
+							yes = false;
+						}
+					} 
 				
+					
+				} // j 반목문 끝
+				
+				
+				if (yes) {
 				if (left + right == 0) {
 					System.out.println("YES");
 				} else {
 					System.out.println("NO");
+				}
 				}
 				// 초기화
 				left = 1;
