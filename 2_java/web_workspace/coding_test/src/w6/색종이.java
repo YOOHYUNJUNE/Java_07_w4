91bcab739e6bf8699379b5677ab46d00a04d29a8
유현준 <140572350+YOOHYUNJUNE@users.noreply.github.com>
2024-08-07 19:01:57 +0900

    0807

--- 2_java/web_workspace/coding_test/src/w6/색종이.java
+++ 2_java/web_workspace/coding_test/src/w6/색종이.java
@@ -1,7 +1,15 @@
 package w6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class 색종이 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		// 색이 서로 다른 직사각형 색종이 n장 순서대로
 		// n장 색종이가 주어진 위치에 차례로 놓일 때, 각 색종이가 보이는 부분의 면적
@@ -11,12 +19,60 @@ public class 색종이 {
 		// [1] 0 0 10 10 : 100 -> 64
 		// [2] 2 2 6 6 : 36
 		
-		// 겹치는 경우
-		// n1(a1 b1 c1 d1), n2(a2 b2 c2 d2), n3(a3 b3 c3 d3)....
-		// a1 < a2
+		
+		// 좌표를 어떻게 표시할 것인가 : 리스트?
+		// 0 0 2 2 : [ {0,0}, {0,1}, {0,2}, {1,0}, {1,1}, {1,2}, {2,0}, {2,1}, {2,2} ]
+		// for문으로 리스트에 좌표 전부 넣기
+		
+		// 색종이 수 n에 따라 겹치는 부분은 지우기 : 겹치는 부분 삭제는 어떻게?
+			// for문 돌며 겹치는 부분이 있으면 n-1 색종이의 좌표 삭제
+		
+		// 마지막 색종이 a b c d 에서 c * d 출력
 		
 		
 		
+		/////////////제현님 풀이
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+		// 좌표 전부를 설정
+		int[][] paper = new int[1001][1001]; 
+		
+		for (int i=1; i<n+1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			int h = Integer.parseInt(st.nextToken());
+
+			// 2차원 배열 [y~h][x~h] -> 각 좌표들을 i로 지정
+			for (int j=y; j<y+h; j++) {
+				for (int k=x; k<x+w; k++) {
+					paper[j][k] = i;
+				}
+			}
+			
+		}
+		
+	
+		for (int i=1; i<n+1; i++) {
+			int count = 0; // 넓이
+			
+			// 완전 탐색
+			for (int j=0; j<1001; j++) {
+				for (int k=0; k<1001; k++) {
+					// 좌표가 i랑 일치하면 넓이로 포함시켜서 더함
+					if (paper[j][k] == i) {
+						count += 1;
+					};
+				}
+			}			
+			System.out.println(count);
+		
+		}
 		
 	}
 }
--- 2_java/web_workspace/coffeemenu/src/main/java/com/coffee/model/Coffee.java
+++ 2_java/web_workspace/coffeemenu/src/main/java/com/coffee/model/Coffee.java
@@ -14,3 +14,24 @@ public class Coffee {
 	public Coffee(String name, String img, int price, int caffeine, int sugar, String detail) {
 	}
 }
+
+
+//# coffee_db 생성
+//drop database if exists coffee_db;
+//
+//create database coffee_db default character set = 'utf8mb4' collate utf8mb4_0900_ai_ci;
+//
+//use coffee_db;
+//
+//# 테이블 생성
+//create table coffee (
+//	id int primary key auto_increment,
+//	name varchar(255) not null,
+//	img varchar(255) not null,
+//	price int not null,
+//	caffeine int not null default 0,
+//	sugar int not null default 0,
+//	detail varchar(255) not null 
+//);
+//
+//select * from coffee;
