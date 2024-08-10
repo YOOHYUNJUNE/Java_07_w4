2ac3bddc779460f59a61581304e576fc1564ffd6
YOOHYUNJUN <rnlayrnlay@gmail.com>
2024-08-10 19:09:26 +0900

    0810

--- 2_java/web_workspace/coding_test/src/w7/체스판.java
+++ 2_java/web_workspace/coding_test/src/w7/체스판.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class 체스판 {
 	public static void main(String[] args) {
 		
-		// m 가로 * n 세로 (>=8) 체스판 W,B 입력 
+		// n 가로 * m 세로 (>=8) 체스판 W,B 입력 
 		// WB 또는 BW가 아니면 +1
 		
 		// 체스판 입력
@@ -17,8 +17,15 @@ public class 체스판 {
 		
 		int m = Integer.parseInt(parts[0]);
 		int n = Integer.parseInt(parts[1]);
+		int count = 0;
 		
-		
+		for (int i=0; i<m; i++) { // m: 세로
+			System.out.println("체스 줄 입력");
+			String line = sc.nextLine(); // WBWBWB 등 : n과 길이가 같아야 함.
+			
+			// 색칠할 필요가 없는 경우 ; 
+			
+		}
 		
 		
 		
